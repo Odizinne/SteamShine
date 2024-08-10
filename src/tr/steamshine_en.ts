6de3bb352f08498681d16060d73b5832211da5d3
Odizinne <flora.aubry@proton.me>
2024-08-10 10:26:05 +0200

    added en tr

--- src/tr/steamshine_en.ts
+++ src/tr/steamshine_en.ts
@@ -1,83 +1,83 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-  <context>
+<TS version="2.1" language="en_US">
+<context>
     <name>MainWindow</name>
     <message>
-      <location filename="..\steamshine.py" line="77" />
-      <source>Settings</source>
-      <translation type="unfinished" />
+        <location filename="../steamshine.py" line="77"/>
+        <source>Settings</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\steamshine.py" line="78" />
-      <source>Quit</source>
-      <translation type="unfinished" />
+        <location filename="../steamshine.py" line="78"/>
+        <source>Quit</source>
+        <translation></translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>SteamShine</name>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>SteamShine - Settings</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>SteamShine - Settings</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Path settings</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Path settings</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Browse</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Browse</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Sunshine apps.json</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Sunshine apps.json</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Steamapps folder</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Steamapps folder</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>General settings</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>General settings</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Check rate (seconds)</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Check rate (seconds)</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Run at startup</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Run at startup</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Start SteamShine</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Start SteamShine</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Advanced mode will try to bind to launched game executable,
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Advanced mode will try to bind to launched game executable,
 allowing to quit stream on game ending and vice versa.
 This is experimental and might not work everytime.</source>
-      <translation type="unfinished" />
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Advanced mode ⚠️</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Advanced mode ⚠️</source>
+        <translation></translation>
     </message>
     <message>
-      <location filename="..\designer\ui_mainwindow.ui" line="0" />
-      <source>Managed Games</source>
-      <translation type="unfinished" />
+        <location filename="../designer/ui_mainwindow.ui" line="0"/>
+        <source>Managed Games</source>
+        <translation></translation>
     </message>
-  </context>
+</context>
 </TS>
